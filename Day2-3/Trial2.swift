1dd915e44f6a649cb84353edebe4f56ee613021c
Prajwal8705 <75818734+Prajwal8705@users.noreply.github.com>
2021-01-01 15:51:07 -0500

    Project 1

--- Day2-3/Trial2.swift
+++ Day2-3/Trial2.swift
@@ -1,3 +1,13 @@
 var Reads = true
-var able = "Hello this is my first and it's /(Reads) that I read"
+var able = "Hello this is my first and it's \(Reads) that I read"
 print(able)
+var names: [String] = ["Bob", "Harry", "buliding"]
+print(names[1])
+var name = "Bob"
+
+enum Buildings { 
+    case tower(height: Int)
+    case skyscraper(height: Double)
+}
+var Burj = Buildings.skyscraper(height: 112.0)
+print(Burj)
