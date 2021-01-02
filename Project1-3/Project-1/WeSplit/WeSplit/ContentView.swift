1df71857b4f03409931b954f8898bc7aaa0594c8
Prajwal8705 <75818734+Prajwal8705@users.noreply.github.com>
2021-01-01 19:54:38 -0500

    Project 1

---
+++ Project1-3/Project-1/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Guruprasad Rao on 12/30/20.
+//
+ 
+import SwiftUI
+ 
+struct ContentView: View {
+   @State private var checkAmount = ""
+   @State private var numberOfPeople = 0
+   @State private var tipPercentage = 2
+  
+   let tipPercentages = [10, 15, 20, 25, 0]
+  
+   var totalPerPerson: Double {
+       let peopleCount = Double(numberOfPeople + 2)
+       let tipSelection = Double(tipPercentages[tipPercentage])
+       let orderAmount = Double(checkAmount) ?? 0
+      
+       let grandTotal = orderAmount * (1 + (tipSelection/100))
+       let amountPerPerson = grandTotal / peopleCount
+       return amountPerPerson
+      
+   }
+  
+   var body: some View {
+       NavigationView {
+           Form {
+               Section {
+                   TextField("Amount", text : $checkAmount)
+                       .keyboardType(.decimalPad)
+                   Picker("What is the Number of People?", selection : $numberOfPeople){
+                       ForEach(2 ..< 100){
+                           Text(" \($0) people")
+                       }
+                   }
+               }
+               Section(header: Text("What tip Percentage would you Like?")){
+                   Picker("What Tip Percentage?", selection : $tipPercentage){
+                       ForEach(0 ..< tipPercentages.count){
+                           Text("\(self.tipPercentages[$0])%")
+                       }
+                   }
+                   .pickerStyle(SegmentedPickerStyle())
+               }
+               Section {
+                   Text("Total Amount Per Person is $\(String(format: "%.2f",totalPerPerson))")
+               }
+           }
+           .navigationBarTitle("WeSplit")
+          
+       }
+      
+   }
+}
+// Building App
+struct ContentView_Previews: PreviewProvider {
+   static var previews: some View {
+       ContentView()
+   }
+}
+ 
+
---
+++ Project1-3/Project-1/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Guruprasad Rao on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
